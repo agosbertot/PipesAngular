04bb0c9718c104823358ecd4cd75a594dd80b386
Agostina <agos.bertot@gmail.com>
2020-06-30 17:05:51 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = 'Capitan America';
+  arreglo: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI: number = Math.PI;
+  porcentaje = 0.234;
+  salario = 1234.5;
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'primera',
+      casa: 20
+    }
+  };
+
+  valorPromesa = new Promise<string>((resolve) => {
+    setTimeout( () => {
+resolve('llego la data');
+    }, 4500);
+  });
+
+
 }
